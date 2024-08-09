23c69570598615b8577090e24bdefb637c135ca4
Tapiwa <tapiwachimbwanda4@gmail.com>
2024-08-09 10:01:31 -0400

    post a tour API completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const app = express();
 
+app.use(express.json());
+
 /* app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from the server", app: "Natours" });
 });
@@ -11,11 +13,13 @@ app.post("/", (req, res) => {
   res.send("You can post ...");
 }); */
 
+// Read tours data
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/tours", (req, res) => {
+// Get all tours API
+app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "successful",
     results: tours.length,
@@ -23,6 +27,35 @@ app.get("/tours", (req, res) => {
       tours,
     },
   });
+});
+
+//Get a tour based on a url parameter
+app.get("/api/v1/tours/:id", (req, res) => {
+  res.status(200).json({
+    status: "successful",
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
+//Post a tour
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return console.log(err);
+      res
+        .status(201)
+        .json({ status: "Added successfully", data: req.body, ID: newId });
+    }
+  );
 });
 
 const port = 3000;
